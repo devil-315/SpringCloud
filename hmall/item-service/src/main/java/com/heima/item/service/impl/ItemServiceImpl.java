f4d1a0dd4f0c75e05e6115cb7a9d3e96de873de6
devil <3223291384@qq.com>
2025-01-06 14:41:13 +0800

    服务保护和分布式事务

--- hmall/item-service/src/main/java/com/heima/item/service/impl/ItemServiceImpl.java
+++ hmall/item-service/src/main/java/com/heima/item/service/impl/ItemServiceImpl.java
@@ -10,6 +10,7 @@ import com.hmall.common.exception.BizIllegalException;
 import com.hmall.common.utils.BeanUtils;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.List;
@@ -25,6 +26,7 @@ import java.util.List;
 public class ItemServiceImpl extends ServiceImpl<ItemMapper, Item> implements IItemService {
 
     @Override
+    @Transactional
     public void deductStock(List<OrderDetailDTO> items) {
         String sqlStatement = "com.heima.item.mapper.ItemMapper.updateStock";
         boolean r = false;
